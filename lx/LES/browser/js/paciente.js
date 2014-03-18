a15e45df80c35c1e1222f3c6c52ad9a4c8c4e86f
andreclimaco <andre.climaco@gmail.com>
2014-03-18 11:22:13 -0300

    JS mascara campo paciente

---
+++ lx/LES/browser/js/paciente.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $("#cep_paciente").mask("99.999-999");
+    $("#fone_paciente").mask("(99) 9999-9999");
+    $("#cel_paciente").mask("(99) 9999-9999");
+    $("#fone_parente_paciente").mask("(99) 9999-9999");
+    //$("#").mask("");
+    //$("#").mask("");
+    //$("#").mask("");
+    //$("#").mask("");
+    //$("#").mask("");
+});
