4b1e36d7ce37b79de78ff11e5a8fcf23b00df39d
andreclimaco <andre.climaco@gmail.com>
2014-04-01 16:05:50 -0300

    searchBox

--- lx/LES/browser/layers.py
+++ lx/LES/browser/layers.py
@@ -1,6 +1,15 @@
+# -*- coding: utf-8 -*-
 
+# Product imports
 from plone.theme.interfaces import IDefaultPloneLayer
+from zope.viewlet.interfaces import IViewletManager
+
 
 class ILXDefaultLayer(IDefaultPloneLayer):
+    """
+    """
+
+
+class ISearchHomeViewletManager(IViewletManager):
     """
     """
